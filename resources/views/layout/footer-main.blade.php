8afef4e4fb9da8772fb5165a9bf6d6ba42e119da
Djempol2022 <djempol2022@gmail.com>
2023-02-21 13:46:49 +0700

    Hapus File Lama

--- resources/views/layout/footer-main.blade.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    {{-- PADDING BOTTOM DASHBOARD 407px / 25.438rem --}}
-    <div class="footer mb-0" style="margin-top: 200px">
-        <div class="justify-content-center d-flex">
-            <p>2022 &copy; Hak Cipta LSP SMK 1 Sintang</p>
-        </div>
-    </div>
-</footer>
