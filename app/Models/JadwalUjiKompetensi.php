ae4b28d04617422ca9aa0297f263c5a842b379dd
Hendra Afrizal <hendraafrizal30@gmail.com>
2023-03-04 11:26:38 +0700

    d

--- app/Models/JadwalUjiKompetensi.php
+++ app/Models/JadwalUjiKompetensi.php
@@ -18,35 +18,48 @@ class JadwalUjiKompetensi extends Model
         return $this->belongsTo(MateriUjiKompetensi::class, 'muk_id', 'id');
     }
 
-    public function relasi_user_asesor(){
+    public function relasi_asesor_uji_kompetensi()
+    {
+        return $this->hasMany(AsesorUjiKompetensi::class, 'jadwal_uji_kompetensi_id', 'id');
+    }
+
+    public function relasi_user_asesor()
+    {
         return $this->belongsTo(AsesorUjiKompetensi::class, 'id', 'jadwal_uji_kompetensi_id');
     }
 
-    public function relasi_user_login_asesor(){
+    public function relasi_user_login_asesor()
+    {
         return $this->belongsTo(AsesorUjiKompetensi::class, 'id', 'jadwal_uji_kompetensi_id')->where('user_asesor_id', Auth::user()->id);
     }
-    
-    public function relasi_user_peninjau(){
+
+    public function relasi_user_peninjau()
+    {
         return $this->belongsTo(PeninjauUjiKompetensi::class, 'id', 'jadwal_uji_kompetensi_id');
     }
 
-    public function relasi_user_asesi(){
+    public function relasi_user_asesi()
+    {
         return $this->belongsTo(AsesiUjiKompetensi::class, 'id', 'jadwal_uji_kompetensi_id');
     }
 
-    public function relasi_user_login_asesi(){
+    public function relasi_user_login_asesi()
+    {
         return $this->belongsTo(AsesiUjiKompetensi::class, 'id', 'jadwal_uji_kompetensi_id')->where('user_asesi_id', Auth::user()->id);
     }
 
-    public function relasi_pelaksanaan_ujian(){
+    public function relasi_pelaksanaan_ujian()
+    {
         return $this->belongsTo(PelaksanaanUjian::class, 'id', 'jadwal_uji_kompetensi_id');
     }
 
-    public function relasi_soal(){
+    public function relasi_soal()
+    {
         return $this->belongsTo(Soal::class, 'id', 'jadwal_uji_kompetensi_id')->orderBy('id', 'desc');
     }
 
-    public function relasi_status_koreksi(){
+    public function relasi_status_koreksi()
+    {
         return $this->belongsTo(KoreksiJawaban::class, 'jadwal_uji_kompetensi_id', 'id');
     }
 }
